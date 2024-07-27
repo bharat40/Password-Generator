811ef6b584b3bfa4ee738c12f6ae6ca3036af4c4
bharat <bharatdhiman40@gmail.com>
2024-07-27 15:49:37 +0530

    container-centered

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import Generator from "./Components/Generator";
 
 function App() {
   return (
-    <div className="bg-gray-900 w-full h-screen">
-      <h1 className="text-4xl text-white text-center">Password Generator</h1>
+    <div className="bg-gray-900 w-full h-screen flex flex-col items-center gap-48">
+      <h1 className="text-4xl text-white ">Password Generator</h1>
       <Generator />
     </div>
   );
